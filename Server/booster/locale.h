8d45494d00357e650f46948ec2815c5d25562fd2
Chris5407 <christopher.smith4202@gmail.com>
2019-04-29 11:41:46 -0500

    Including cppcms libararies

---
+++ Server/booster/locale.h
@@ -0,0 +1,27 @@
+//
+//  Copyright (c) 2009-2011 Artyom Beilis (Tonkikh)
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef BOOSTER_LOCALE_H_INCLUDED
+#define BOOSTER_LOCALE_H_INCLUDED
+
+#include "locale/boundary.h"
+#include "locale/collator.h"
+#include "locale/conversion.h"
+#include "locale/date_time.h"
+#include "locale/date_time_facet.h"
+#include "locale/encoding.h"
+#include "locale/format.h"
+#include "locale/formatting.h"
+#include "locale/generator.h"
+#include "locale/gnu_gettext.h"
+#include "locale/info.h"
+#include "locale/localization_backend.h"
+#include "locale/message.h"
+#include "locale/util.h"
+
+#endif
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
---
+++ Server/server.cpp
@@ -0,0 +1,10 @@
+#include "cppcms/application.h" 
+#include "cppcms/applications_pool.h"
+#include "cppcms/service.h" 
+#include "cppcms/http_response.h"
+#include <iostream>
+
+int main() {
+
+	return 0;
+}
