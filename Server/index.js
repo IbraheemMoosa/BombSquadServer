be43a47e61bcb92e9277e5079714c06eb9e13e1b
ChrisSmith2 <christopher.smith4202@gmail.com>
2019-05-13 10:45:28 -0500

    Got web server running

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
+var path = require('path');
 var exphbs  = require('express-handlebars');
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var handlebars = exphbs.create({
+	extname: '.html'
+});
+app.set('views', path.join(__dirname, '/../Website'));
+app.engine('html', handlebars.engine);
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, '/../Website')));
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('homepage');
 });
 
-app.listen(3000);
+app.listen(8080);
