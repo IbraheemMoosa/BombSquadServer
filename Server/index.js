e04f90c7936f509d8e553e84c9f46eec7fde14e9
ChrisSmith2 <christopher.smith4202@gmail.com>
2019-05-17 10:21:03 -0500

    File display page working

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var exphbs  = require('express-handlebars');
 var multer = require('multer');
+var fs = require('fs');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -44,7 +45,9 @@ app.get('/download/:filename', function (req, res) {
 });
 
 app.get('/files', function(req, res) {
-	res.render('files');
+	var fileNames = fs.readdirSync(__dirname + "/uploads/");
+	fileNames.splice(fileNames.indexOf('.gitignore'), 1);
+	res.render('files', {fileNames: fileNames});
 });
 
 app.post('/upload', upload.single("file"), function (req, res, next) {
