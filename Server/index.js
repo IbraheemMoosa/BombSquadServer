0187b1e272e87acc7b5127dea59e800915cd10aa
ChrisSmith2 <christopher.smith4202@gmail.com>
2019-05-17 11:31:17 -0500

    Search finished

--- Server/index.js
+++ Server/index.js
@@ -3,13 +3,15 @@ var path = require('path');
 var exphbs  = require('express-handlebars');
 var multer = require('multer');
 var fs = require('fs');
+var findInFiles = require('find-in-files');
+const escapeStringRegexp = require('escape-string-regexp');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+	cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, req.body.filename+path.extname(file.originalname));
+	cb(null, req.body.filename+path.extname(file.originalname));
   }
 })
 
@@ -28,16 +30,16 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, '/../Website')));
 
 app.get('/', function (req, res) {
-    res.render('homepage');
+	res.render('homepage');
 });
 
 app.get('/file/:filename', function (req, res) {
 	var filename = req.params.filename;
 	var imgregex = /\.(jpg|jpeg|png|gif)$/i;
-    res.render('page template', {
-    	filename: filename, 
-    	isImage: imgregex.test(filename)
-    });
+	res.render('page template', {
+		filename: filename, 
+		isImage: imgregex.test(filename)
+	});
 });
 
 app.get('/download/:filename', function (req, res) {
@@ -46,12 +48,21 @@ app.get('/download/:filename', function (req, res) {
 
 app.get('/files', function(req, res) {
 	var fileNames = fs.readdirSync(__dirname + "/uploads/");
-	fileNames.splice(fileNames.indexOf('.gitignore'), 1);
 	if (!req.query.q) {
+		fileNames.splice(fileNames.indexOf('.gitignore'), 1);
 		res.render('files', {fileNames: fileNames});
 	} else {
-		
-		res.render('files', {fileNames: fileNames});
+		var search = escapeStringRegexp(req.query.q);
+		var nameMatches = fileNames.filter(word => word.match(search));
+		findInFiles.find(search, './uploads', '\.(txt|py|cpp|h|java|html|css|js|json)$').then(function(results) {
+			var contentMatches = Object.keys(results);
+			contentMatches = contentMatches.map(function(name) {
+				return name.split('\\').pop();
+			});
+			var matchedFiles = contentMatches.concat(nameMatches);
+			console.log("Search matched:", matchedFiles);
+			res.render('files', {fileNames: matchedFiles});
+		});
 	}
 });
 
